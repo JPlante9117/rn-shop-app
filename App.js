d1b0ef549767dc72cea170dac5c600995a78054a
Jacques Plante <jacrplante@gmail.com>
2020-04-24 14:07:56 -0400

    set up base redux store in App.js

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import productsReducer from './store/reducers/productsReducer';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+})
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>STUFF</Text>
+      </View>
+    </Provider>
   );
 }
 
