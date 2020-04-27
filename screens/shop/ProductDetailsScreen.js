6b85276246f7fabe3e9cd17352f3a6f589e026d8
Jacques Plante <jacrplante@gmail.com>
2020-04-27 14:28:02 -0400

    displayed content on product details screen

--- screens/shop/ProductDetailsScreen.js
+++ screens/shop/ProductDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, Image, Button, StyleSheet, ScrollView } from 'react-native'
 import { useSelector } from 'react-redux'
+import Colors from '../../constants/Colors'
 
 const ProductDetailsScreen = props => {
 
@@ -11,13 +12,41 @@ const ProductDetailsScreen = props => {
 
     return(
         <View>
-            <Text>{selectedProduct.title}</Text>
+            <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title="Add To Cart" onPress={() => {}} />
+            </View>
+            <ScrollView style={styles.textDetails}>
+                <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+                <Text style={styles.description}>{selectedProduct.description}</Text>
+            </ScrollView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: '100%',
+        height: 300
+    },
+    textDetails: {
+        margin: 10
+    },
+    price: {
+        color: '#888',
+        fontSize: 20,
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center'
+    },
+    actions: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10
+    }
 })
 
 export const productDetailsOptions = navData => {
