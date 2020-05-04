9a8b32ffc4bad0bb81d23f79bbd3549e9c849d18
Jacques Plante <jacrplante@gmail.com>
2020-05-04 10:30:38 -0400

    added autofocus to next field on return press

--- screens/user/EditProductsScreen.js
+++ screens/user/EditProductsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { StyleSheet, View, Platform } from 'react-native'
 import DefaultText from '../../components/UI/DefaultText'
 import { ScrollView, TextInput } from 'react-native-gesture-handler'
@@ -16,6 +16,9 @@ const EditProductsScreen = props => {
     const [imgURL, setImgURL] = useState(currentProduct ? currentProduct.imageUrl : '')
     const [price, setPrice] = useState(currentProduct ? currentProduct.price.toString() : '')
     const [desc, setDesc] = useState(currentProduct ? currentProduct.description : '')
+    const inputImgUrl = useRef(null)
+    const inputPrice = useRef(null)
+    const inputDesc = useRef(null)
     const disablePrice = currentProduct ? true : false
 
     const dispatch = useDispatch()
@@ -33,17 +36,53 @@ const EditProductsScreen = props => {
         props.navigation.setParams({submitForm: submitHandler})
     }, [submitHandler])
 
+    const nextClickHandler = (gotoRef) => {
+        gotoRef.current.focus()
+    }
+
     return(
         <ScrollView style={styles.form}>
-            <View style={styles.formControl}>
+            <View key={1} style={styles.formControl}>
                 <DefaultText style={styles.label}>Title:</DefaultText>
-                <TextInput style={styles.input} placeholder={'Enter the Product Name'} value={title} onChangeText={(text) => setTitle(text)} />
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Enter the Product Name'}
+                    value={title}
+                    onChangeText={(text) => setTitle(text)}
+                    autoCapitalize='words'
+                    returnKeyType='next'
+                    onSubmitEditing={() => nextClickHandler(inputImgUrl)}
+                />
                 <DefaultText style={styles.label}>Image URL:</DefaultText>
-                <TextInput style={styles.input} placeholder={'Enter an Image URL for the Product'} value={imgURL} onChangeText={(text) => setImgURL(text)} />
+                <TextInput
+                    ref={inputImgUrl}
+                    style={styles.input}
+                    placeholder={'Enter an Image URL for the Product'}
+                    value={imgURL}
+                    onChangeText={(text) => setImgURL(text)}
+                    returnKeyType='next'
+                    onSubmitEditing={() => nextClickHandler(inputPrice)}
+                />
                 <DefaultText style={styles.label}>Price:</DefaultText>
-                <TextInput style={!disablePrice ? styles.input : styles.disabledInput} placeholder={'Enter the Price for the Product'} value={price} onChangeText={(text) => setPrice(text)} editable={!disablePrice} />
+                <TextInput
+                    ref={inputPrice}
+                    style={!disablePrice ? styles.input : styles.disabledInput}
+                    placeholder={'Enter the Price for the Product'}
+                    value={price}
+                    onChangeText={(text) => setPrice(text)}
+                    editable={!disablePrice}
+                    keyboardType='decimal-pad'
+                    returnKeyType='next'
+                    onSubmitEditing={() => nextClickHandler(inputDesc)}
+                />
                 <DefaultText style={styles.label}>Description:</DefaultText>
-                <TextInput style={styles.input} placeholder={'Enter a short Description of the Product'} value={desc} onChangeText={(text) => setDesc(text)} />
+                <TextInput
+                    ref={inputDesc}
+                    style={styles.input}
+                    placeholder={'Enter a short Description of the Product'}
+                    value={desc}
+                    onChangeText={(text) => setDesc(text)}
+                />
             </View>
         </ScrollView>
     )
