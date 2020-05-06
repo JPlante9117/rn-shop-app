272c8cb96d09f3d112ff3910fc489549f10bc156
Jacques Plante <jacrplante@gmail.com>
2020-05-06 15:21:50 -0400

    added logout button

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer'
 import ProductsOverviewScreen, { productOverviewOptions } from '../screens/shop/ProductsOverviewScreen'
 import Colors from '../constants/Colors'
-import { Platform } from 'react-native'
+import { View, Platform, Button, ScrollView } from 'react-native'
 import { enableScreens } from 'react-native-screens'
 import ProductDetailsScreen, { productDetailsOptions } from '../screens/shop/ProductDetailsScreen'
 import OrdersScreen, { ordersOptions } from '../screens/shop/OrdersScreen'
@@ -12,6 +12,11 @@ import { Ionicons, Entypo } from '@expo/vector-icons'
 import UserProductsScreen, {userProductsOptions} from '../screens/user/UserProductsScreen'
 import EditProductsScreen, { editProductsOptions } from '../screens/user/EditProductsScreen'
 import AuthScreen, {authScreenOptions} from '../screens/user/AuthScreen'
+import { useDispatch } from 'react-redux'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { LOGOUT, logout } from '../store/actions/authActions'
+import DefaultText from '../components/UI/DefaultText'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 enableScreens()
 
@@ -92,6 +97,8 @@ export const ShopNavigator = props => {
             </Stack.Navigator>
         )
     }
+
+    const dispatch = useDispatch()
     
     return(
             <Drawer.Navigator
@@ -99,19 +106,35 @@ export const ShopNavigator = props => {
                 drawerContentOptions={{
                     activeTintColor: Colors.primary
                 }}
+                drawerStyle={{
+                    width: '60%'
+                }}
+                drawerContent={props => {
+                    return(
+                        <ScrollView contentContainerStyle={{flex: 1, justifyContent: 'space-between'}}>
+                            <SafeAreaView forceInset={{top: 'always', horizontal: 'never'}}>
+                                <DrawerItemList {...props} />
+                            </SafeAreaView>
+                            <TouchableOpacity style={{padding: 10, flexDirection: 'row', backgroundColor: Colors.primary, alignItems: 'center'}} onPress={() => dispatch(logout())}>
+                                    <Ionicons name={Platform.OS === 'android' ? 'md-exit' : 'ios-exit'} size={23} color='white' />
+                                    <DefaultText style={{color: 'white', marginLeft: 30}}>LOGOUT</DefaultText>
+                            </TouchableOpacity>
+                        </ScrollView>
+                    )
+                }}
             >
                 <Drawer.Screen
                     name="Shop"
                     component={storeStack}
                     options={{
-                        drawerIcon: drawerConfig => <Entypo name={'shop'} size={23} color={drawerConfig.tintColor} />
+                        drawerIcon: props => <Entypo name={'shop'} size={23} color={props.color} />
                     }}
                 />
                 <Drawer.Screen
                     name="Orders"
                     component={orderStack}
                     options={{
-                        drawerIcon: drawerConfig => <Ionicons name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} size={23} color={drawerConfig.tintColor}/>
+                        drawerIcon: props => <Ionicons name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} size={23} color={props.color}/>
                     }}
                     
                 />
@@ -119,7 +142,7 @@ export const ShopNavigator = props => {
                     name="User"
                     component={userStack}
                     options={{
-                        drawerIcon: drawerConfig => <Ionicons name={Platform.OS === 'android' ? 'md-create' : 'ios-create'} size={23} color={drawerConfig.tintColor}/>,
+                        drawerIcon: props => <Ionicons name={Platform.OS === 'android' ? 'md-create' : 'ios-create'} size={23} color={props.color}/>,
                         drawerLabel: 'My Store'
                     }}
                 />
